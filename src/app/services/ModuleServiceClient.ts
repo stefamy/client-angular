dac1ac4ce3fe3a1f86a3122edc163c61abd6b9f2
Amy Stefani <amystefani1@gmail.com>
2020-04-15 16:03:45 -0700

    updating fetch urls to https

--- src/app/services/ModuleServiceClient.ts
+++ src/app/services/ModuleServiceClient.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class ModuleServiceClient {
   findModulesForCourse = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/stefamy4405/courses/${courseId}/modules`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/stefamy4405/courses/${courseId}/modules`)
       .then(response => response.json())
 }
