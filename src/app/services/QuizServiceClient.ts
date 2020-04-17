3da44c649ed49fcb0a39404f484d1b6b5efdff57
Amy Stefani <amystefani1@gmail.com>
2020-04-17 14:25:09 -0700

    updating service urls for heroku whiteboard server

--- src/app/services/QuizServiceClient.ts
+++ src/app/services/QuizServiceClient.ts
@@ -3,13 +3,13 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findQuizById = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    fetch(`https://whiteboard-node-server-cs5610.herokuapp.com/api/quizzes/${qid}`)
     .then(response => response.json())
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    fetch(`https://whiteboard-node-server-cs5610.herokuapp.com/api/quizzes/${qid}/questions`)
     .then(response => response.json())
   submitQuiz = (qid, questions) => {
-    fetch(`http://localhost:3000/api/quizzes/${qid}/attempts`, {
+    fetch(`https://whiteboard-node-server-cs5610.herokuapp.com/api/quizzes/${qid}/attempts`, {
       method: 'POST',
       body: JSON.stringify(questions), headers: {
         'content-type': 'application/json'
